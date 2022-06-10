80508ef45473dd52231b5e77fe002c0d3df063f3
Sebastian Muszynski <s.muszynski@constructiva.de>
2022-06-10 15:32:15 +0200

    Improve dump config naming

--- components/dps/number/dps_number.cpp
+++ components/dps/number/dps_number.cpp
@@ -6,10 +6,10 @@ namespace dps {
 
 static const char *const TAG = "dps.number";
 
+void DpsNumber::dump_config() { LOG_NUMBER("", "DPS Number", this); }
 void DpsNumber::control(float value) {
   this->parent_->write_register(this->holding_register_, (uint16_t)(value * (10.0f / this->traits.get_step())));
 }
-void DpsNumber::dump_config() { LOG_NUMBER(TAG, "DPS Number", this); }
 
 }  // namespace dps
 }  // namespace esphome
