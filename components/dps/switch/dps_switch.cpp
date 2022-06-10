05acc358d5c95056033999d2bb4cbef173dee9d3
Sebastian Muszynski <s.muszynski@constructiva.de>
2022-06-10 12:53:36 +0200

    Revert change

--- components/dps/switch/dps_switch.cpp
+++ components/dps/switch/dps_switch.cpp
@@ -7,7 +7,7 @@ namespace dps {
 
 static const char *const TAG = "dps.switch";
 
-void DpsSwitch::dump_config() { LOG_SWITCH("", "Dps Switch", this); }
+void DpsSwitch::dump_config() { LOG_SWITCH("", "DPS Switch", this); }
 void DpsSwitch::write_state(bool state) { this->parent_->write_register(this->holding_register_, (uint16_t) state); }
 
 }  // namespace dps
