80844423c6192e2af6b9186e93adf20ea198cc2e
Sebastian Muszynski <basti@linkt.de>
2023-07-04 07:45:53 +0200

    Make number entity current resolution aware

--- components/dps/number/dps_number.cpp
+++ components/dps/number/dps_number.cpp
@@ -8,7 +8,13 @@ static const char *const TAG = "dps.number";
 
 void DpsNumber::dump_config() { LOG_NUMBER("", "DPS Number", this); }
 void DpsNumber::control(float value) {
-  this->parent_->write_register(this->holding_register_, (uint16_t) (value * (1.0f / this->traits.get_step())));
+  float resolution = 1.0f;
+
+  if (this->holding_register_ == 0x0001) {
+    resolution = 0.01f / this->parent_->current_resolution_factor();
+  }
+
+  this->parent_->write_register(this->holding_register_, (uint16_t) (value * (resolution / this->traits.get_step())));
 }
 
 }  // namespace dps
