eaeaa686be0f4f915c7f5ed5264e7e10fbd87928
Sebastian Muszynski <basti@linkt.de>
2026-03-26 06:05:20 +0100

    Add state_class to sensor schemas (#57)

--- components/lazy_limiter/sensor.py
+++ components/lazy_limiter/sensor.py
@@ -1,7 +1,12 @@
 import esphome.codegen as cg
 from esphome.components import sensor
 import esphome.config_validation as cv
-from esphome.const import DEVICE_CLASS_POWER, ICON_EMPTY, UNIT_WATT
+from esphome.const import (
+    DEVICE_CLASS_POWER,
+    ICON_EMPTY,
+    STATE_CLASS_MEASUREMENT,
+    UNIT_WATT,
+)
 
 from . import CONF_LAZY_LIMITER_ID, LAZY_LIMITER_COMPONENT_SCHEMA
 
@@ -17,6 +22,7 @@ CONFIG_SCHEMA = LAZY_LIMITER_COMPONENT_SCHEMA.extend(
             icon=ICON_EMPTY,
             accuracy_decimals=0,
             device_class=DEVICE_CLASS_POWER,
+            state_class=STATE_CLASS_MEASUREMENT,
         ),
     }
 )
